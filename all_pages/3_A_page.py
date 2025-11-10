a2d02a090b84f4bc4db6c4a6334043a5a31062bd
Javier Torres <javiercamilotorresvera@gmail.com>
2025-11-10 18:41:56 -0500

    ejemplo datos de la sesion

--- all_pages/3_A_page.py
+++ all_pages/3_A_page.py
@@ -1,3 +1,13 @@
 import streamlit as st
 
 st.title("Equipo A")
+
+'FREE'
+'PREMIUM'
+st.session_state.token = "token inventado"
+st.session_state.is_authenticated = True
+st.session_state.membership = 'PREMIUM'
+
+st.info(
+    f"{st.session_state.token}{st.session_state.is_authenticated}{st.session_state.membership}"
+)
